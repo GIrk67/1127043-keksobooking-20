d2ef2ca7abb705bc2c34bba061315e4e6fefee2e
Irina Guseva <i0645495@gmail.com>
2020-06-17 23:31:35 +0200

    Личный проект: больше деталей(правки 01)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 'use strict';
 
-var AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+var AVATAR = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+];
 
-var TITLE = ['Дивный дворец', 'Чудесная квартирка', 'Милый дом', 'Уютное бунгало'];
+var TITLES = [
+  'Дивный дворец',
+  'Чудесная квартирка',
+  'Милый дом',
+  'Уютное бунгало',
+];
 
 // var ADDRESS = [];
 
@@ -18,21 +32,33 @@ var CHECKIN = ['12:00', '13:00', '14:00'];
 
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
 
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
 
 var LOCATION = {
   x: getRandomIntElement(130, 1130),
-  y: getRandomIntElement(130, 630)
+  y: getRandomIntElement(130, 630),
 };
 
-var showMap = document.querySelector('.map');
-showMap.classList.remove('map--faded');
+var mapElement = document.querySelector('.map');
+mapElement.classList.remove('map--faded');
 
 var mapPin = document.querySelector('.map__pins');
-var similarMapPin = mapPin.querySelector('#pin')
-  .content.querySelector('map__pin');
+var similarMapPin = document
+  .querySelector('#pin')
+  .content.querySelector('.map__pin');
 
 //  Функция случайного выбора из массива
 
@@ -50,30 +76,47 @@ function getRandomIntElement(min, max) {
 
 // Создание массива объектов
 
-var getInformArrow = function () {
-  var informarrow = [{
-    author: getRandomArrElement(AVATAR),
-    offer: {
-      title: getRandomArrElement(TITLE),
-      price: getRandomArrElement(PRICE),
-      type: getRandomArrElement(TYPE),
-      rooms: getRandomArrElement(ROOMS),
-      guests: getRandomArrElement(GUESTS),
-      checkin: getRandomArrElement(CHECKIN),
-      checkout: getRandomArrElement(CHECKOUT),
-      features: getRandomArrElement(FEATURES),
-      photos: getRandomArrElement(PHOTOS)
-    },
-    location: getRandomArrElement(LOCATION)
-  }];
+var getInformArrow = function (number) {
+  for (var i = 0; i < number; i++) {
+    var informarrow = [
+      {
+        author: {
+          avatar: getRandomArrElement(AVATAR),
+        },
+        offer: {
+          title: getRandomArrElement(TITLES),
+          price: getRandomArrElement(PRICE),
+          type: getRandomArrElement(TYPE),
+          rooms: getRandomArrElement(ROOMS),
+          guests: getRandomArrElement(GUESTS),
+          checkin: getRandomArrElement(CHECKIN),
+          checkout: getRandomArrElement(CHECKOUT),
+          features: getRandomArrElement(FEATURES),
+          photos: getRandomArrElement(PHOTOS),
+        },
+        location: getRandomArrElement(LOCATION),
+      },
+    ];
+  }
   return informarrow;
 };
 
+var inform = getInformArrow(8);
+
 var renderMapPin = function (informarrow) {
   var mapPinElement = similarMapPin.cloneNode(true);
-  mapPinElement.querySelector('img').src = informarrow.author;
+  mapPinElement.querySelector('img').src = informarrow.author.avatar;
   mapPinElement.querySelector('img').alt = informarrow.offer.title;
-  mapPinElement.style = 'top: informarrow.location(y); left: ininformarrow.location(y)';
+  mapPinElement.style =
+    'top: informarrow.location(y); left: ininformarrow.location(y)';
 
   return mapPinElement;
 };
+
+renderMapPin();
+
+var pinElement = document.createDocumentFragment();
+for (var i = 0; i < inform.length; i++) {
+  pinElement.appendChild(renderMapPin(inform[i]));
+}
+mapPin.appendChild(pinElement);
